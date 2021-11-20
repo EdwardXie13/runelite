523a74538f6be47ea72e6e244a646d8ee9db64e0
EdwardXie <edwardxie1@gmail.com>
2021-11-20 10:54:24 -0500

    updates to RunecraftPlus

--- runelite-client/src/main/java/net/runelite/client/plugins/runecraftPlus/DenseRunestoneOverlay.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/runecraftPlus/DenseRunestoneOverlay.java
@@ -52,7 +52,6 @@ public class DenseRunestoneOverlay extends Overlay
     private static final WorldPoint middleArea = new WorldPoint(1737, 3875, 0);
     private static final WorldPoint runZone = new WorldPoint(1707, 3860, 0);
     private static final WorldPoint bloodZone = new WorldPoint(1717, 3835,0);
-    private static final WorldPoint bloodAltarCenter = new WorldPoint(1717, 3830, 0);
     private static final WorldPoint returnZone = new WorldPoint(1738, 3852, 0);
     private static final WorldPoint centerOfMine = new WorldPoint(1761, 3860, 0);
 
@@ -171,7 +170,6 @@ public class DenseRunestoneOverlay extends Overlay
                     renderTileArea(graphics, LocalPoint.fromWorld(client, runZone));
                 }
             }
-
             //if at run zone, render little area above blood altar
             else if(getInventorySlotID(27) == 13446 && isNearWorldTile(runZone, 2)) {
                 if(config.rotateCamera() && client.getMapAngle() != cameraRunZone) {
@@ -181,7 +179,7 @@ public class DenseRunestoneOverlay extends Overlay
                 renderTileArea(graphics, LocalPoint.fromWorld(client, bloodZone));
             }
             //render blood altar if near it
-            else if((getInventorySlotID(0) == 7938 || getInventorySlotID(27) == 13446) && isNearWorldTile(bloodAltarCenter, 3) && bloodAltar != null) {
+            else if((getInventorySlotID(0) == 7938 || getInventorySlotID(27) == 13446) && isNearWorldTile(bloodZone, 2) && bloodAltar != null) {
                 renderBox(graphics, bloodAltar);
             }
             //if at blood altar spot render return zone
