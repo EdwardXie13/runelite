d7db96d9ad589047c7a8f83fba77705923236762
EdwardXie <edwardxie1@gmail.com>
2021-11-24 13:06:11 -0500

    updates

--- runRunelite.bat
+++ runRunelite.bat
@@ -1,3 +1,3 @@
 @echo off 
-start javaw -jar -ea ./runelite-client/target/client-1.8.4-SNAPSHOT-shaded.jar --debug --developer-mode 
+start javaw -jar -ea ./runelite-client/target/client-1.8.5-SNAPSHOT-shaded.jar --debug --developer-mode 
 exit
