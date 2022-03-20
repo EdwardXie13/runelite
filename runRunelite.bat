1591395d3383969b2ec28bb40ba56bc1e735ab0d
EdwardXie <edwardxie1@gmail.com>
2022-03-20 12:52:52 -0400

    bump

--- runRunelite.bat
+++ runRunelite.bat
@@ -1,4 +1,4 @@
 @echo off 
-start javaw -jar -ea ./runelite-client/target/client-1.8.14-SNAPSHOT-shaded.jar --debug --developer-mode
+start javaw -jar -ea ./runelite-client/target/client-1.8.15-SNAPSHOT-shaded.jar --developer-mode
 python "2. adjustWin.py"
 exit
