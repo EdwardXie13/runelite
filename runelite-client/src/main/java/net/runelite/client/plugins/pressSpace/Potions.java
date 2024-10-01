902a6eb00f2ec267cd232aec75a2426abb4a3730
EdwardXie <edwardxie1@gmail.com>
2024-10-01 08:53:21 -0500

    Update Potions.java

--- runelite-client/src/main/java/net/runelite/client/plugins/pressSpace/Potions.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/pressSpace/Potions.java
@@ -15,35 +15,38 @@ public class Potions {
         Arrays.asList(
             // unf pot + ingredient
             Set.of(Pair.of(ItemID.GUAM_POTION_UNF, 14), Pair.of(ItemID.EYE_OF_NEWT, 14)),
-                Set.of(Pair.of(ItemID.HARRALANDER_POTION_UNF, 27), Pair.of(ItemID.VOLCANIC_ASH, 1)),
-                Set.of(Pair.of(ItemID.TARROMIN_POTION_UNF, 14), Pair.of(ItemID.LIMPWURT_ROOT, 14)),
-                Set.of(Pair.of(ItemID.TARROMIN_POTION_UNF, 14), Pair.of(ItemID.ASHES, 14)),
-                Set.of(Pair.of(ItemID.RANARR_POTION_UNF, 14), Pair.of(ItemID.SNAPE_GRASS, 14)),
-                Set.of(Pair.of(ItemID.TOADFLAX_POTION_UNF, 14), Pair.of(ItemID.CRUSHED_NEST, 14)),
-                Set.of(Pair.of(ItemID.IRIT_POTION_UNF, 14), Pair.of(ItemID.EYE_OF_NEWT, 14)),
-                Set.of(Pair.of(ItemID.IRIT_POTION_UNF, 14), Pair.of(ItemID.UNICORN_HORN_DUST, 14)),
-                Set.of(Pair.of(ItemID.AVANTOE_POTION_UNF, 14), Pair.of(ItemID.MORT_MYRE_FUNGUS, 14)),
-                Set.of(Pair.of(ItemID.AVANTOE_POTION_UNF, 14), Pair.of(ItemID.SNAPE_GRASS, 14)),
-                Set.of(Pair.of(ItemID.KWUARM_POTION_UNF, 14), Pair.of(ItemID.LIMPWURT_ROOT, 14)),
-                Set.of(Pair.of(ItemID.SNAPDRAGON_POTION_UNF, 14), Pair.of(ItemID.RED_SPIDERS_EGGS, 14)),
-                Set.of(Pair.of(ItemID.CADANTINE_POTION_UNF, 14), Pair.of(ItemID.WHITE_BERRIES, 14)),
-                Set.of(Pair.of(ItemID.CADANTINE_POTION_UNF, 14), Pair.of(ItemID.WINE_OF_ZAMORAK, 14)),
-                Set.of(Pair.of(ItemID.CADANTINE_POTION_UNF, 14), Pair.of(ItemID.POTATO_CACTUS, 14)),
-                Set.of(Pair.of(ItemID.LANTADYME_POTION_UNF, 14), Pair.of(ItemID.DRAGON_SCALE_DUST, 14)),
-                Set.of(Pair.of(ItemID.DWARF_WEED_POTION_UNF, 14), Pair.of(ItemID.WINE_OF_ZAMORAK, 14)),
-                Set.of(Pair.of(ItemID.DWARF_WEED_POTION_UNF, 14), Pair.of(ItemID.NIHIL_DUST, 14)),
-                Set.of(Pair.of(ItemID.TORSTOL_POTION_UNF, 14), Pair.of(ItemID.JANGERBERRIES, 14)),
+            Set.of(Pair.of(ItemID.HARRALANDER_POTION_UNF, 27), Pair.of(ItemID.VOLCANIC_ASH, 1)),
+            Set.of(Pair.of(ItemID.HARRALANDER_POTION_UNF, 14), Pair.of(ItemID.CHOCOLATE_DUST, 14)),
+            Set.of(Pair.of(ItemID.TARROMIN_POTION_UNF, 14), Pair.of(ItemID.LIMPWURT_ROOT, 14)),
+            Set.of(Pair.of(ItemID.TARROMIN_POTION_UNF, 14), Pair.of(ItemID.ASHES, 14)),
+            Set.of(Pair.of(ItemID.RANARR_POTION_UNF, 14), Pair.of(ItemID.SNAPE_GRASS, 14)),
+            Set.of(Pair.of(ItemID.TOADFLAX_POTION_UNF, 14), Pair.of(ItemID.CRUSHED_NEST, 14)),
+            Set.of(Pair.of(ItemID.IRIT_POTION_UNF, 14), Pair.of(ItemID.EYE_OF_NEWT, 14)),
+            Set.of(Pair.of(ItemID.IRIT_POTION_UNF, 14), Pair.of(ItemID.UNICORN_HORN_DUST, 14)),
+            Set.of(Pair.of(ItemID.AVANTOE_POTION_UNF, 14), Pair.of(ItemID.MORT_MYRE_FUNGUS, 14)),
+            Set.of(Pair.of(ItemID.AVANTOE_POTION_UNF, 14), Pair.of(ItemID.SNAPE_GRASS, 14)),
+            Set.of(Pair.of(ItemID.KWUARM_POTION_UNF, 14), Pair.of(ItemID.LIMPWURT_ROOT, 14)),
+            Set.of(Pair.of(ItemID.SNAPDRAGON_POTION_UNF, 14), Pair.of(ItemID.RED_SPIDERS_EGGS, 14)),
+            Set.of(Pair.of(ItemID.CADANTINE_POTION_UNF, 14), Pair.of(ItemID.WHITE_BERRIES, 14)),
+            Set.of(Pair.of(ItemID.LANTADYME_POTION_UNF, 14), Pair.of(ItemID.DRAGON_SCALE_DUST, 14)),
+            Set.of(Pair.of(ItemID.LANTADYME_POTION_UNF, 14), Pair.of(ItemID.POTATO_CACTUS, 14)),
+            Set.of(Pair.of(ItemID.DWARF_WEED_POTION_UNF, 14), Pair.of(ItemID.WINE_OF_ZAMORAK, 14)),
+            Set.of(Pair.of(ItemID.DWARF_WEED_POTION_UNF, 14), Pair.of(ItemID.NIHIL_DUST, 14)),
+            Set.of(Pair.of(ItemID.TORSTOL_POTION_UNF, 14), Pair.of(ItemID.JANGERBERRIES, 14)),
 
             // pot + ingredient
-                Set.of(Pair.of(ItemID.SUPER_ENERGY3, 27), Pair.of(ItemID.AMYLASE_CRYSTAL, 1)),
-                Set.of(Pair.of(ItemID.SUPER_ENERGY4, 27), Pair.of(ItemID.AMYLASE_CRYSTAL, 1)),
-                Set.of(Pair.of(ItemID.ANTIFIRE_POTION4, 14), Pair.of(ItemID.CRUSHED_SUPERIOR_DRAGON_BONES, 14)),
+            Set.of(Pair.of(ItemID.SUPER_ENERGY3, 27), Pair.of(ItemID.AMYLASE_CRYSTAL, 1)),
+            Set.of(Pair.of(ItemID.SUPER_ENERGY4, 27), Pair.of(ItemID.AMYLASE_CRYSTAL, 1)),
+            Set.of(Pair.of(ItemID.ANTIFIRE_POTION4, 14), Pair.of(ItemID.CRUSHED_SUPERIOR_DRAGON_BONES, 14)),
 
-            // super combat test
+            // super combat
                 Set.of(Pair.of(ItemID.TORSTOL, 7),
                         Pair.of(ItemID.SUPER_ATTACK4, 7),
                         Pair.of(ItemID.SUPER_DEFENCE4, 7),
-                        Pair.of(ItemID.SUPER_STRENGTH4, 7))
+                        Pair.of(ItemID.SUPER_STRENGTH4, 7)),
+
+            // quick close bank
+                Set.of(Pair.of(ItemID.CHOCOLATE_BAR, 27), Pair.of(ItemID.PESTLE_AND_MORTAR, 1))
         )
     );
 }
