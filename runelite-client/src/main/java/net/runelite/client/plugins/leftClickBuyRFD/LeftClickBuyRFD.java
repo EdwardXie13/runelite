c733f770fcbe3bfb998fe9b0c99d01de2b1fe97b
EdwardXie <edwardxie1@gmail.com>
2024-11-08 03:20:56 -0600

    Create LeftClickBuyRFD.java

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/leftClickBuyRFD/LeftClickBuyRFD.java
@@ -0,0 +1,164 @@
+package net.runelite.client.plugins.leftClickBuyRFD;
+
+import net.runelite.api.*;
+import net.runelite.api.coords.WorldPoint;
+import net.runelite.api.events.GameTick;
+import net.runelite.api.events.ItemContainerChanged;
+import net.runelite.api.events.MenuEntryAdded;
+import net.runelite.api.widgets.WidgetInfo;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+
+import javax.inject.Inject;
+import java.awt.*;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.util.*;
+import java.util.List;
+
+@PluginDescriptor(name = "Left Click Buy RFD")
+public class LeftClickBuyRFD extends Plugin {
+    @Inject
+    private Client client;
+
+    public List<Item> inventoryItems = new ArrayList<>();
+
+    public ItemContainer inventoryContainer = null;
+
+    private int chocolateBarCount = 0;
+    private int grapeCount = 0;
+
+    @Subscribe
+    public void onGameTick(GameTick event) {
+        if (isBankOpen() &&
+            getCountInventoryItems() == 1 &&
+            containsItem(ItemID.COINS_995)) {
+            pressKey(KeyEvent.VK_ESCAPE);
+        }
+        else if (isRFDChestOpen() &&
+                ((  getWidgetCount(19660816, 1) == 0 &&
+                    getWidgetCount(19660816, 5) == 0) ||
+                getCountInventoryItems() == 28)) {
+            pressKey(KeyEvent.VK_ESCAPE);
+
+            if(chocolateBarCount == 0 && grapeCount == 0)
+                quickHop();
+        }
+    }
+
+    @Subscribe
+    public void onMenuEntryAdded(MenuEntryAdded menuEntryAdded)
+    {
+        if(Objects.equals(client.getLocalPlayer().getWorldLocation(), new WorldPoint(3218, 9623, 0))) {
+            swapMenus();
+        }
+    }
+
+    @Subscribe
+    public void onItemContainerChanged(ItemContainerChanged event) {
+        if (event.getItemContainer() == client.getItemContainer(InventoryID.INVENTORY)) {
+            inventoryContainer = event.getItemContainer();
+        }
+    }
+
+
+    public void swapMenus() {
+        MenuEntry entry = client.getMenuEntries()[client.getMenuEntries().length - 1];
+        String target = entry.getTarget();
+        if(target != null && target.contains("Chest")) {
+            if(getCountInventoryItems() == 28) {
+                swapBank();
+            } else if(getCountInventoryItems() < 28) {
+                swapBuyFood();
+            }
+        }
+    }
+
+    private void swapBank() {
+        MenuEntry[] menuEntries = client.getMenuEntries();
+        for (int i = menuEntries.length - 1; i >= 0; --i) {
+            MenuEntry entry = menuEntries[i];
+            if (entry.getOption().equals("Bank"))
+            {
+                menuEntries[i] = menuEntries[menuEntries.length - 1];
+                menuEntries[menuEntries.length - 1] = entry;
+
+                client.setMenuEntries(menuEntries);
+                break;
+            }
+        }
+    }
+
+    private void swapBuyFood() {
+        MenuEntry[] menuEntries = client.getMenuEntries();
+        for (int i = menuEntries.length - 1; i >= 0; --i) {
+            MenuEntry entry = menuEntries[i];
+            if (entry.getOption().equals("Buy-food"))
+            {
+                menuEntries[i] = menuEntries[menuEntries.length - 1];
+                menuEntries[menuEntries.length - 1] = entry;
+
+                client.setMenuEntries(menuEntries);
+                break;
+            }
+        }
+    }
+
+    private void pressKey(int key) {
+        KeyEvent keyPress = new KeyEvent(this.client.getCanvas(), KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, key, KeyEvent.CHAR_UNDEFINED);
+        this.client.getCanvas().dispatchEvent(keyPress);
+        KeyEvent keyRelease = new KeyEvent(this.client.getCanvas(), KeyEvent.KEY_RELEASED, System.currentTimeMillis(), 0, key, KeyEvent.CHAR_UNDEFINED);
+        this.client.getCanvas().dispatchEvent(keyRelease);
+    }
+
+    private int getCountInventoryItems() {
+        inventoryItems = Arrays.asList(inventoryContainer.getItems());
+        return inventoryContainer.count();
+    }
+
+    private boolean containsItem(int item) {
+        return inventoryItems.stream().anyMatch(
+                items -> items.getId() == item
+        );
+    }
+
+    private int getWidgetCount(int widgetId, int index) {
+        try {
+            int quantity = client.getWidget(widgetId).getChild(index).getItemQuantity();
+            if(index == 1)
+                chocolateBarCount = quantity;
+            else if(index == 5)
+                grapeCount = quantity;
+            return quantity;
+        } catch(Exception ignored) {
+            return 0;
+        }
+    }
+
+    private boolean isBankOpen() {
+        return client.getWidget(WidgetInfo.BANK_CONTAINER) != null;
+    }
+
+    private boolean isRFDChestOpen() {
+        return client.getWidget(WidgetInfo.SHOP_INVENTORY_ITEMS_CONTAINER) != null;
+    }
+
+    private void quickHop() {
+        try {
+            Robot robot = new Robot();
+
+            // Press and hold Ctrl, Shift, and Left Arrow
+            robot.keyPress(KeyEvent.VK_CONTROL);
+            robot.keyPress(KeyEvent.VK_SHIFT);
+            robot.keyPress(KeyEvent.VK_LEFT);
+
+            // Release Left Arrow, Shift, and Ctrl
+            robot.keyRelease(KeyEvent.VK_LEFT);
+            robot.keyRelease(KeyEvent.VK_SHIFT);
+            robot.keyRelease(KeyEvent.VK_CONTROL);
+
+        } catch (Exception ignored){
+        }
+    }
+}
