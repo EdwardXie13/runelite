c891e75e389ade04f4e47b19cef207de99785008
EdwardXie <edwardxie1@gmail.com>
2025-01-12 17:29:08 -0600

    Update Potions.java

--- runelite-client/src/main/java/net/runelite/client/plugins/pressSpace/Potions.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/pressSpace/Potions.java
@@ -40,41 +40,41 @@ public class Potions {
             Set.of(Pair.of(ItemID.ANTIFIRE_POTION4, 14), Pair.of(ItemID.CRUSHED_SUPERIOR_DRAGON_BONES, 14)),
 
             // super combat
-                Set.of(Pair.of(ItemID.TORSTOL, 7),
-                        Pair.of(ItemID.SUPER_ATTACK4, 7),
-                        Pair.of(ItemID.SUPER_DEFENCE4, 7),
-                        Pair.of(ItemID.SUPER_STRENGTH4, 7)),
+            Set.of(Pair.of(ItemID.TORSTOL, 7),
+                    Pair.of(ItemID.SUPER_ATTACK4, 7),
+                    Pair.of(ItemID.SUPER_DEFENCE4, 7),
+                    Pair.of(ItemID.SUPER_STRENGTH4, 7)),
 
             // quick close bank
-                Set.of(Pair.of(ItemID.CHOCOLATE_BAR, 27), Pair.of(ItemID.PESTLE_AND_MORTAR, 1)),
-                Set.of(Pair.of(ItemID.BIRD_NEST_5075, 27), Pair.of(ItemID.PESTLE_AND_MORTAR, 1)),
-                Set.of(Pair.of(ItemID.BUCKET_OF_SAND, 14), Pair.of(ItemID.SODA_ASH, 14)),
-                Set.of(Pair.of(ItemID.MOLTEN_GLASS, 27), Pair.of(ItemID.GLASSBLOWING_PIPE, 1)),
-                Set.of(Pair.of(ItemID.BATTLESTAFF, 14), Pair.of(ItemID.FIRE_ORB, 14)),
+            Set.of(Pair.of(ItemID.CHOCOLATE_BAR, 27), Pair.of(ItemID.PESTLE_AND_MORTAR, 1)),
+            Set.of(Pair.of(ItemID.BIRD_NEST_5075, 27), Pair.of(ItemID.PESTLE_AND_MORTAR, 1)),
+            Set.of(Pair.of(ItemID.BUCKET_OF_SAND, 14), Pair.of(ItemID.SODA_ASH, 14)),
+            Set.of(Pair.of(ItemID.MOLTEN_GLASS, 27), Pair.of(ItemID.GLASSBLOWING_PIPE, 1)),
+            Set.of(Pair.of(ItemID.BATTLESTAFF, 14), Pair.of(ItemID.FIRE_ORB, 14)),
 
-//                Set.of(Pair.of(ItemID.GREEN_DRAGON_LEATHER, 26),Pair.of(ItemID.THREAD, 1), Pair.of(ItemID.NEEDLE, 1)),
-//                Set.of(Pair.of(ItemID.BLUE_DRAGON_LEATHER, 26),Pair.of(ItemID.THREAD, 1), Pair.of(ItemID.NEEDLE, 1)),
-//                Set.of(Pair.of(ItemID.RED_DRAGON_LEATHER, 26),Pair.of(ItemID.THREAD, 1), Pair.of(ItemID.NEEDLE, 1)),
-//                Set.of(Pair.of(ItemID.BLACK_DRAGON_LEATHER, 26),Pair.of(ItemID.THREAD, 1), Pair.of(ItemID.NEEDLE, 1)),
+//            Set.of(Pair.of(ItemID.GREEN_DRAGON_LEATHER, 26),Pair.of(ItemID.THREAD, 1), Pair.of(ItemID.NEEDLE, 1)),
+//            Set.of(Pair.of(ItemID.BLUE_DRAGON_LEATHER, 26),Pair.of(ItemID.THREAD, 1), Pair.of(ItemID.NEEDLE, 1)),
+//            Set.of(Pair.of(ItemID.RED_DRAGON_LEATHER, 26),Pair.of(ItemID.THREAD, 1), Pair.of(ItemID.NEEDLE, 1)),
+//            Set.of(Pair.of(ItemID.BLACK_DRAGON_LEATHER, 26),Pair.of(ItemID.THREAD, 1), Pair.of(ItemID.NEEDLE, 1)),
 
-                Set.of(Pair.of(ItemID.GREEN_DRAGONHIDE, 25),Pair.of(ItemID.NATURE_RUNE, 1), Pair.of(ItemID.ASTRAL_RUNE, 1), Pair.of(ItemID.JUG ,1)),
-                Set.of(Pair.of(ItemID.BLUE_DRAGONHIDE, 25),Pair.of(ItemID.NATURE_RUNE, 1), Pair.of(ItemID.ASTRAL_RUNE, 1), Pair.of(ItemID.JUG ,1)),
-                Set.of(Pair.of(ItemID.RED_DRAGONHIDE, 25),Pair.of(ItemID.NATURE_RUNE, 1), Pair.of(ItemID.ASTRAL_RUNE, 1), Pair.of(ItemID.JUG ,1)),
-                Set.of(Pair.of(ItemID.BLACK_DRAGONHIDE, 25),Pair.of(ItemID.NATURE_RUNE, 1), Pair.of(ItemID.ASTRAL_RUNE, 1), Pair.of(ItemID.JUG ,1)),
+            Set.of(Pair.of(ItemID.GREEN_DRAGONHIDE, 25),Pair.of(ItemID.NATURE_RUNE, 1), Pair.of(ItemID.ASTRAL_RUNE, 1), Pair.of(ItemID.JUG ,1)),
+            Set.of(Pair.of(ItemID.BLUE_DRAGONHIDE, 25),Pair.of(ItemID.NATURE_RUNE, 1), Pair.of(ItemID.ASTRAL_RUNE, 1), Pair.of(ItemID.JUG ,1)),
+            Set.of(Pair.of(ItemID.RED_DRAGONHIDE, 25),Pair.of(ItemID.NATURE_RUNE, 1), Pair.of(ItemID.ASTRAL_RUNE, 1), Pair.of(ItemID.JUG ,1)),
+            Set.of(Pair.of(ItemID.BLACK_DRAGONHIDE, 25),Pair.of(ItemID.NATURE_RUNE, 1), Pair.of(ItemID.ASTRAL_RUNE, 1), Pair.of(ItemID.JUG ,1)),
 
-                Set.of(Pair.of(ItemID.UNCUT_EMERALD, 27),Pair.of(ItemID.CHISEL, 1)),
-                Set.of(Pair.of(ItemID.UNCUT_SAPPHIRE, 27),Pair.of(ItemID.CHISEL, 1)),
-                Set.of(Pair.of(ItemID.UNCUT_RUBY, 27),Pair.of(ItemID.CHISEL, 1)),
-                Set.of(Pair.of(ItemID.UNCUT_DIAMOND, 27),Pair.of(ItemID.CHISEL, 1)),
+            Set.of(Pair.of(ItemID.UNCUT_EMERALD, 27),Pair.of(ItemID.CHISEL, 1)),
+            Set.of(Pair.of(ItemID.UNCUT_SAPPHIRE, 27),Pair.of(ItemID.CHISEL, 1)),
+            Set.of(Pair.of(ItemID.UNCUT_RUBY, 27),Pair.of(ItemID.CHISEL, 1)),
+            Set.of(Pair.of(ItemID.UNCUT_DIAMOND, 27),Pair.of(ItemID.CHISEL, 1)),
 
-                Set.of(Pair.of(ItemID.TEAK_LOGS, 27),Pair.of(ItemID.COINS_995, 1)),
-                Set.of(Pair.of(ItemID.SILVER_BAR, 26), Pair.of(ItemID.SILVER_BOLTS_UNF, 1), Pair.of(ItemID.BOLT_MOULD, 1)),
-                Set.of(Pair.of(ItemID.SILVER_BAR, 27), Pair.of(ItemID.BOLT_MOULD, 1)),
-                Set.of(Pair.of(ItemID.SEAWEED, 28)),
-                Set.of(Pair.of(ItemID.JUG, 28)),
-                Set.of(Pair.of(ItemID.JUG_OF_WATER, 14), Pair.of(ItemID.GRAPES, 14)),
-                Set.of(Pair.of(ItemID.CAKE_TIN, 6), Pair.of(ItemID.BUCKET_OF_MILK, 6), Pair.of(ItemID.POT_OF_FLOUR, 6), Pair.of(ItemID.EGG, 6)),
-                Set.of(Pair.of(ItemID.UNCOOKED_CAKE, 6))
+            Set.of(Pair.of(ItemID.TEAK_LOGS, 27),Pair.of(ItemID.COINS_995, 1)),
+            Set.of(Pair.of(ItemID.SILVER_BAR, 26), Pair.of(ItemID.SILVER_BOLTS_UNF, 1), Pair.of(ItemID.BOLT_MOULD, 1)),
+            Set.of(Pair.of(ItemID.SILVER_BAR, 27), Pair.of(ItemID.BOLT_MOULD, 1)),
+            Set.of(Pair.of(ItemID.SEAWEED, 28)),
+            Set.of(Pair.of(ItemID.JUG, 28)),
+            Set.of(Pair.of(ItemID.JUG_OF_WATER, 14), Pair.of(ItemID.GRAPES, 14)),
+            Set.of(Pair.of(ItemID.CAKE_TIN, 6), Pair.of(ItemID.BUCKET_OF_MILK, 6), Pair.of(ItemID.POT_OF_FLOUR, 6), Pair.of(ItemID.EGG, 6)),
+            Set.of(Pair.of(ItemID.UNCOOKED_CAKE, 6))
         )
     );
 }
