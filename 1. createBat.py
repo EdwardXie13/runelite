013b425e6633caaf08e08d2dfe74dd734f9c6fd1
EdwardXie <edwardxie1@gmail.com>
2026-04-26 19:03:45 -0500

    a

--- 1. createBat.py
+++ 1. createBat.py
@@ -1,23 +1,24 @@
 import os
-import shutil
 
-#create Bat
-batCommand = "@echo off \n" + \
-    "start javaw -jar -ea ./runelite-client/target/{} --debug --developer-mode\n" + \
-    'python "2. adjustWin.py"\n' + \
-    "exit"
+jar_dir = os.path.join(os.getcwd(), "runelite-client", "build", "libs")
 
-for file in os.listdir(os.getcwd() + "/runelite-client/target"):
-    if('shaded' in file):
-        batCommand = batCommand.format(file)
+jar_file = None
+for file in os.listdir(jar_dir):
+    if "shaded" in file and file.endswith(".jar"):
+        jar_file = file
+        break
 
-        f = open("runRunelite.bat", "w")
-        f.write(batCommand)
-        f.close()
+if jar_file is None:
+    raise FileNotFoundError("No shaded jar found in build/libs")
 
+bat_command = (
+    "@echo off\n"
+    f"start javaw -jar -ea ./runelite-client/build/libs/{jar_file} --debug --developer-mode\n"
+    'python "2. adjustWin.py"\n'
+    "exit\n"
+)
 
-#overwrite local settings with cloud
-currentUser = os.path.expanduser('~')
-currentUser += "\.runelite"
+with open("runRunelite.bat", "w") as f:
+    f.write(bat_command)
 
-shutil.copy('./settings.properties', currentUser)
+print(f"Created runRunelite.bat using: {jar_file}")
--- runRunelite.bat
+++ runRunelite.bat
@@ -1,4 +1,4 @@
-@echo off 
-start javaw -jar -ea ./runelite-client/build/libs/client-1.12.24-SNAPSHOT-shaded.jar --debug --developer-mode
+@echo off
+start javaw -jar -ea ./runelite-client/build/libs/client-1.12.25-SNAPSHOT-shaded.jar --debug --developer-mode
 python "2. adjustWin.py"
 exit
