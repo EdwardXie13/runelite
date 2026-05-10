26d9334639070682a3227e55bc80d9ec36ca48bb
EdwardXie <edwardxie1@gmail.com>
2026-05-09 21:47:31 -0500

    Update runRunelite.bat

--- runRunelite.bat
+++ runRunelite.bat
@@ -1,4 +1,4 @@
 @echo off
-start javaw -jar -ea ./runelite-client/build/libs/client-1.12.25-SNAPSHOT-shaded.jar --debug --developer-mode
+start javaw -jar -ea ./runelite-client/build/libs/client-1.12.27-SNAPSHOT-shaded.jar --debug --developer-mode
 python "2. adjustWin.py"
 exit
