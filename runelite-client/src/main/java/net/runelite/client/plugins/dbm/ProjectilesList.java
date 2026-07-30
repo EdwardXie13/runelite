4e063af7067ca167c2ce6eb2d37815bef58b0f83
EdwardXie <edwardxie1@gmail.com>
2026-07-29 22:21:16 -0500

    cox

--- runelite-client/src/main/java/net/runelite/client/plugins/dbm/ProjectilesList.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/dbm/ProjectilesList.java
@@ -8,7 +8,12 @@ public class ProjectilesList {
         Map.entry(1481, new ProjectileInfo("Bomb", 1, 0)), // BOMB
         Map.entry(1482, new ProjectileInfo("Missile", 0, 0)), // ACID MISSLE
         Map.entry(1483, new ProjectileInfo("Acid", 0, 24)), // ACID
-        Map.entry(1484, new ProjectileInfo("Spawn", 0,0)) // ZOMBIE_SPAWN
+        Map.entry(1484, new ProjectileInfo("Spawn", 0,0)), // ZOMBIE_SPAWN
+
+        // OLM
+        Map.entry(1352, new ProjectileInfo("", 1, 0)), // falling crystal
+        Map.entry(1354, new ProjectileInfo("", 1, 0)), // acid spot
+        Map.entry(1357, new ProjectileInfo("", 1, 0)) // falling rock
     );
 
     public static final Map<Integer, ProjectileInfo> GRAPHICS_OBJECTS = Map.ofEntries(
